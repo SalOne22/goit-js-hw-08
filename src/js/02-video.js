0eaac6d6e4ec3d845928af51a17c70b617a211a6
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-28 12:43:54 +0200

    Complete second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayerEl = document.getElementById('vimeo-player');
+const player = new Player(vimeoPlayerEl);
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+window.addEventListener('DOMContentLoaded', updateTimeFromStorage);
+
+const TIME_STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlayerTimeUpdate(time) {
+  localStorage.setItem(TIME_STORAGE_KEY, time.seconds);
+}
+
+function updateTimeFromStorage() {
+  const seconds = Number(localStorage.getItem(TIME_STORAGE_KEY));
+  player.setCurrentTime(seconds);
+}
